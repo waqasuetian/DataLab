7ae224e181bf52703337288e8fcf922d6649fa80
waqasuetian <97124934+waqasuetian@users.noreply.github.com>
2024-09-07 16:25:29 +0500

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,27 +3,51 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-[999] bg-opacity-90 bg-[#2E8B57] backdrop-blur-lg shadow-md border-b-[2px] border-[#00F5D4] transition-all duration-300">
-      <div className="container mx-auto flex justify-between items-center py-3 px-4 md:px-8">
+    <header className="sticky top-0 z-50 bg-opacity-90 bg-[#2E8B57] backdrop-blur-lg shadow-md border-b-2 border-[#00F5D4] transition-all duration-300">
+      <div className="container mx-auto flex justify-between items-center py-4 px-6">
         {/* Logo Section */}
         <div className="flex items-center space-x-3">
           <img
             src="/Favicon/Favicon.png"
             alt="DataLabB Logo"
-            className="h-8 sm:h-10 md:h-12 lg:h-16"
+            className="h-10"
           />
-          <h3 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold text-[#11F5D4]">
+          <h3 className="text-xl font-bold text-white">
             <Link
-              to={"/"}
+              to="/"
               className="hover:text-[#7F78D2] transition-colors duration-300"
             >
               DataLabB Portfolio
             </Link>
           </h3>
         </div>
+
+        {/* Navigation Links */}
+        <nav className="hidden md:flex space-x-8">
+          <Link
+            to="/projects"
+            className="text-lg font-medium text-white hover:text-[#7F78D2] transition duration-300"
+          >
+            Projects
+          </Link>
+          <Link
+            to="/about"
+            className="text-lg font-medium text-white hover:text-[#7F78D2] transition duration-300"
+          >
+            About
+          </Link>
+          <Link
+            to="/contact"
+            className="text-lg font-medium text-white hover:text-[#7F78D2] transition duration-300"
+          >
+            Contact
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 };
 
 export default Header;
+
 
