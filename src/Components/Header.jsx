f8c5f42fcb9cfd8da9c4450fce70094ca3e9e616
waqasuetian <97124934+waqasuetian@users.noreply.github.com>
2024-09-11 22:22:50 +0500

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="sticky top-0 z-50 bg-opacity-90 bg-[#2E8B57] backdrop-blur-lg shadow-md border-b-2 border-[#00F5D4] transition-all duration-300">
       <div className="container mx-auto flex justify-between items-center py-4 px-6">
       
-          <h3 className="text-xl font-bold text-white">
+          <h3 className="text-xl font-bold text-black">
             <Link
               to="/"
               className="hover:text-[#7F78D2] transition-colors duration-300"
